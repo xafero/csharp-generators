eef8b94a0a1259ad68d159113342586667326992
xafero <xafero@users.noreply.github.com>
2024-02-24 02:04:24 +0100

    Setting up the whole thing

---
+++ src/SqlPreparer/Sources.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis.Text;
+
+namespace SqlPreparer
+{
+    public static class Sources
+    {
+        public static string GenerateAttr(string name, string space = "Generator", string target = "Class")
+        {
+            var code = new StringBuilder();
+            code.AppendLine($"namespace {space}");
+            code.AppendLine("{");
+            code.AppendLine($"\t[System.AttributeUsage(System.AttributeTargets.{target})]");
+            code.AppendLine($"\tpublic class {name}Attribute : System.Attribute");
+            code.AppendLine("\t{");
+            code.AppendLine("\t}");
+            code.AppendLine("}");
+            return code.ToString();
+        }
+
+        public static SourceText From(string source)
+        {
+            var text = SourceText.From(source, Encoding.UTF8);
+            return text;
+        }
+    }
+}
---
+++ src/SqlPreparer/SqlGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SqlPreparer
+{
+    [Generator(LanguageNames.CSharp)]
+    public sealed class SqlGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(ctx =>
+            {
+                var attrCode = Sources.GenerateAttr("Funny");
+                ctx.AddSource("FunnyAttribute.g.cs", Sources.From(attrCode));
+
+
+
+                /*
+                IncrementalValuesProvider<ClassDeclarationSyntax> enumDeclarations = context.SyntaxProvider
+                    .CreateSyntaxProvider(
+                        predicate: static (s, _) => IsSyntaxTargetForGeneration(s),
+                        transform: static (ctx, _) => GetTargetForGeneration(ctx));
+                IncrementalValueProvider<(Compilation, ImmutableArray<ClassDeclarationSyntax>)> compilationAndEnums
+                    = context.CompilationProvider.Combine(enumDeclarations.Collect());
+                context.RegisterSourceOutput(compilationAndEnums,
+                    (spc, source) =>
+                        Execute(source.Item1, source.Item2, spc));
+                */
+            });
+        }
+
+        public void Initialize3(IncrementalGeneratorInitializationContext initContext)
+        {
+            // define the execution pipeline here via a series of transformations:
+
+            // find all additional files that end with .txt
+            var textFiles =
+                initContext.AdditionalTextsProvider.Where(static file => file.Path.EndsWith(".txt"));
+
+            // read their contents and save their name
+            var namesAndContents =
+                textFiles.Select((text, cancellationToken) => (name: Path.GetFileNameWithoutExtension(text.Path),
+                    content: text.GetText(cancellationToken)!.ToString()));
+
+            // generate a class that contains their values as const strings
+            initContext.RegisterSourceOutput(namesAndContents, (spc, nameAndContent) =>
+            {
+                spc.AddSource($"ConstStrings.{nameAndContent.name}", $@"
+    public static partial class ConstStrings
+    {{
+        public const string {nameAndContent.name} = ""{nameAndContent.content}"";
+    }}");
+            });
+        }
+
+        public void Initialize2(IncrementalGeneratorInitializationContext context)
+        {
+            var classProvider = context.SyntaxProvider
+                .CreateSyntaxProvider((node, _) => { return node is ClassDeclarationSyntax; },
+                    (ctx, _) => { return (ClassDeclarationSyntax)ctx.Node; });
+
+            context.RegisterSourceOutput(classProvider, Generate);
+        }
+
+        private static void Generate(SourceProductionContext ctx, ClassDeclarationSyntax cds)
+        {
+            Generate(ctx, cds.Identifier.Text);
+        }
+
+        private static void Generate(SourceProductionContext ctx, string name)
+        {
+            var ns = "DemoConsoleApplication";
+
+            ctx.AddSource($"{ns}.{name}.perf.cs", $@"//
+
+namespace {ns}
+{{
+   partial class {name}
+   {{
+   }}
+}}
+");
+        }
+
+        public static bool IsSyntaxTargetForGeneration(SyntaxNode syntaxNode)
+        {
+            return syntaxNode is ClassDeclarationSyntax classDeclarationSyntax &&
+                   classDeclarationSyntax.AttributeLists.Count > 0 &&
+                   classDeclarationSyntax.AttributeLists
+                       .Any(al => al.Attributes.Any(a => a.Name.ToString() == "GenerateService"));
+        }
+
+        public static ClassDeclarationSyntax GetTargetForGeneration(GeneratorSyntaxContext context)
+        {
+            var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
+            return classDeclarationSyntax;
+        }
+    }
+}
