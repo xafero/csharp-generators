8474dd2abfb65a214e44985fcbfb8a3320b54383
xafero <xafero@users.noreply.github.com>
2024-02-24 09:17:04 +0100

    Finished very first draft

--- src/Cscg.Core/Typing.cs
+++ src/Cscg.Core/Typing.cs
@@ -7,7 +7,14 @@ namespace Cscg.Core
         public static string Parse(TypeSyntax type)
         {
             var name = type.GetText().ToString().TrimNull();
-            return name;
+            switch (name)
+            {
+                case "float": return nameof(System.Single);
+                case "string": return nameof(System.String);
+                case "int": return nameof(System.Int32);
+                case "bool": return nameof(System.Boolean);
+                default: return $"_{name}";
+            }
         }
     }
 }
