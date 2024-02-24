df8e5f14d96cfe0e0107ef00cbd2055df9adafb7
xafero <xafero@users.noreply.github.com>
2024-02-24 04:40:30 +0100

    Got it working

--- demo/SourceGenerated/Program.cs
+++ demo/SourceGenerated/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using Generator;
+using autogen;
 
 namespace SourceGenerated
 {
     [Funny]
     internal static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-           // Console.WriteLine(ConstStrings.Dufk);
+            Console.WriteLine(ConstStrings.Greeting1);
         }
     }
 }
