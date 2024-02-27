b123690b1ccd7bf5d4ec28ea2be37b7fbea52b00
xafero <xafero@users.noreply.github.com>
2024-02-27 08:17:30 +0100

    Adding deep dish

---
+++ src/Cscg.Core/Deep.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Cscg.Core
+{
+    public static class Deep
+    {
+        public static IEnumerable<ClassDeclarationSyntax> SearchType(this ITypeSymbol type,
+            IModuleSymbol[] modules = null)
+        {
+            var typeName = type.ToTrimDisplay();
+            var module = new[] { type.ContainingModule }.Concat(modules ?? []);
+            foreach (var loc in module.SelectMany(m => m.Locations))
+                if (loc.SourceTree is var tree && tree.TryGetRoot(out var root))
+                    if (root is CompilationUnitSyntax cs)
+                        foreach (var member in cs.Members)
+                            if (member is NamespaceDeclarationSyntax nds)
+                                foreach (var child in nds.Members)
+                                    if (child is ClassDeclarationSyntax cds)
+                                        if (cds.BaseList != null)
+                                            foreach (var cn in cds.BaseList.ChildNodes())
+                                            {
+                                                var cnt = cn.ToFullString().TrimNull();
+                                                var fqn = $"{cds.GetParentName()}.{cnt}";
+                                                if (cnt == typeName || fqn == typeName)
+                                                    yield return cds;
+                                            }
+        }
+    }
+}
