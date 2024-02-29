98131d8ac872038dba0d488746c5bc39241cd64e
xafero <xafero@users.noreply.github.com>
2024-02-29 06:17:48 +0100

    Added one attribut for compact generator

--- src/Cscg.Compactor/CompactGenerator.cs
+++ src/Cscg.Compactor/CompactGenerator.cs
@@ -7,6 +7,13 @@ using System.Globalization;
 using System.Linq;
 using Cscg.Core;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cscg.Core;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Xml.Linq;
 using static Cscg.Core.Sources;
 
 namespace Cscg.Compactor
@@ -14,17 +21,18 @@ namespace Cscg.Compactor
     [Generator(LanguageNames.CSharp)]
     public sealed class CompactGenerator : IIncrementalGenerator
     {
-        public void Initialize(IncrementalGeneratorInitializationContext context)
+        private const string Space = Coding.AutoNamespace;
+        private const string BinObjName = "Compacted";
+        private const string ExtObjName = "CompactedExt";
+        private const string IntObjName = "ICompacted";
+
+        public void Initialize(IncrementalGeneratorInitializationContext igi)
         {
-
-
-
-
-
-
-
-
-
+            igi.RegisterPostInitializationOutput(ctx =>
+            {
+                var attrCode = Coding.GenerateAttr(BinObjName, Space);
+                ctx.AddSource($"{BinObjName}Attribute.g.cs", From(attrCode));
+            });
         }
     }
 }
