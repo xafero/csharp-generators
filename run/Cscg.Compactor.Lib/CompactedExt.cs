201530d99aa4c39ce0fc015e50bb23332aa700a1
xafero <xafero@users.noreply.github.com>
2024-02-29 23:16:02 +0100

    Added bytes helper

--- run/Cscg.Compactor.Lib/CompactedExt.cs
+++ run/Cscg.Compactor.Lib/CompactedExt.cs
@@ -1,6 +1,23 @@
-﻿namespace Cscg.Compactor.Lib
+﻿using System.IO;
+
+namespace Cscg.Compactor.Lib
 {
     public static class CompactedExt
     {
+        public static byte[] ToBytes(this Stream stream)
+        {
+            byte[] array;
+            if (stream is MemoryStream mem)
+            {
+                array = mem.ToArray();
+            }
+            else
+            {
+                using var copy = new MemoryStream();
+                stream.CopyTo(copy);
+                array = copy.ToArray();
+            }
+            return array;
+        }
     }
 }
