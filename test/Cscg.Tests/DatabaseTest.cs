bf07493e7e68c31b8bd48b1e1b029fa82a5f3851
xafero <xafero@users.noreply.github.com>
2024-03-14 16:39:56 +0100

    Adding funny test

--- test/Cscg.Tests/DatabaseTest.cs
+++ test/Cscg.Tests/DatabaseTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Data;
 using System.Linq;
 using Cscg.AdoNet.Lib;
@@ -5,6 +6,7 @@ using Microsoft.Data.Sqlite;
 using SourceGenerated.Sql;
 using Xunit;
 using Xunit.Abstractions;
+using IAD = Cscg.AdoNet.Lib.IActiveData<Microsoft.Data.Sqlite.SqliteDataReader, Microsoft.Data.Sqlite.SqliteCommand>;
 using static Cscg.Tests.Tools.DebugTool;
 
 namespace Cscg.Tests
@@ -19,28 +21,73 @@ namespace Cscg.Tests
         }
 
         [Theory]
-        [InlineData("b", "e")]
+        [InlineData("p", "1")]
+        [InlineData("f", "1")]
         public void TestSqlite(string cla, string mode)
+        {
+            if (cla == "p")
+            {
+                DoDatabase(cla, mode, () => new Person { Name = "Tom" },
+                    (x, conn) => x.Id = x.Insert(conn),
+                    (x, conn) =>
+                    {
+                        x.Name = "Harry Potter";
+                        return x.Update(conn);
+                    },
+                    Person.Find, conn => Person.List(conn, 10, 0).Single(),
+                    (conn, y) => Person.FindSame(conn, x => x.Name = y.Name).Single(),
+                    (conn, y) => y.Delete(conn)
+                );
+                return;
+            }
+            if (cla == "f")
+            {
+                DoDatabase(cla, mode, () => new Funny
+                    {
+                        H = 42, P = "Tom", T = 289, A = true, B = 129, C = [3, 4, 22, 43], 
+                        E = DateOnly.FromDateTime(DateTime.Now), F = new DateTime(2011,2,3),
+                        D = 't', G = DateTimeOffset.UnixEpoch, I = 39.2933, K = 2830, J = Guid.NewGuid(),
+                        L = 292, N = -13, M = 39383, O = 99.99223f, U = 39399, S = 2911,
+                        Q = TimeOnly.FromDateTime(DateTime.UtcNow), R = TimeSpan.FromMinutes(29)
+                    },
+                    (x, conn) => x.Id = x.Insert(conn),
+                    (x, conn) =>
+                    {
+                        x.P = "Harry";
+                        return x.Update(conn);
+                    },
+                    Funny.Find, conn => Funny.List(conn, 10, 0).Single(),
+                    (conn, y) => Funny.FindSame(conn, x => x.P = y.P).Single(),
+                    (conn, y) => y.Delete(conn)
+                );
+            }
+        }
+
+        private void DoDatabase<T>(string cla, string mode, Func<T> create,
+            Func<T, SqliteConnection, int> getId, Func<T, SqliteConnection, bool> getUpdate,
+            Func<SqliteConnection, int, T> find, Func<SqliteConnection, T> list,
+            Func<SqliteConnection, T, T> same, Func<SqliteConnection, T, bool> delete)
+            where T : IAD
         {
             var fileName = IoTool.DeleteIfExists($"blog_{cla}_{mode}.db");
             using var conn = CreateConnection(fileName);
 
-            var input = new Person { Name = "Tom" };
+            var input = create();
 
-            var id = input.Id = input.Insert(conn);
+            var id = getId(input, conn);
             Assert.Equal(1, id);
 
-            input.Name = "Harry Potter";
-            Assert.True(input.Update(conn));
+            var upd = getUpdate(input, conn);
+            Assert.True(upd);
 
-            var output = Person.Find(conn, id);
+            var output = find(conn, id);
             Assert.NotNull(output);
 
-            output = Person.List(conn, 10, 0).Single();
+            output = list(conn);
             Assert.NotNull(output);
 
-            output = Person.FindSame(conn, x => x.Name = input.Name).Single();
-            Assert.True(output.Delete(conn));
+            output = same(conn, input);
+            Assert.True(delete(conn, output));
 
             var expected = ToJson(input);
             var actual = ToJson(output);
@@ -58,8 +105,9 @@ namespace Cscg.Tests
             var conn = AdoTool.OpenConn<SqliteConnection>(connStr);
             Assert.Equal(ConnectionState.Open, conn.State);
 
-            var (sql, rows) = conn.RunTransaction(Person.CreateTable());
-            Assert.Equal(172, sql.Length);
+            var (sql, rows) = conn.RunTransaction(Person.CreateTable(),
+                Funny.CreateTable());
+            Assert.Equal(816, sql.Length);
             Assert.Equal(0, rows);
 
             return conn;
