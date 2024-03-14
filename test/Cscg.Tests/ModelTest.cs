4b1db335baf92731b4125cbed33e65fa43411333
xafero <xafero@users.noreply.github.com>
2024-03-14 13:28:10 +0100

    Adding model test

---
+++ test/Cscg.Tests/ModelTest.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.IO;
+using Cscg.Core.Model;
+using Cscg.Tests.Tools;
+using Xunit;
+
+namespace Cscg.Tests
+{
+    public class ModelTest
+    {
+        [Fact]
+        public void TestSample()
+        {
+            const string tmp = "model_sample.xml";
+
+            var cTypes = new CTypes
+            {
+                Types =
+                [
+                    new()
+                    {
+                        Name = "Sample", Kind = TypeKind.Class, Parent = "Testing",
+                        Members =
+                        [
+                            new CMember
+                            {
+                                Name = "Size", Type = "int", Kind = MemberKind.Property,
+                                Attributes = [new CAttribute { Type = "Key", Params = [] }]
+                            }
+                        ],
+                        Attributes =
+                        [
+                            new()
+                            {
+                                Type = "Description", Params = [new() { Value = "42", Name = "Count" }]
+                            }
+                        ]
+                    },
+                    new()
+                    {
+                        Name = "Rest", Kind = TypeKind.Class, Parent = "Super",
+                        Members = [],
+                        Attributes = []
+                    }
+                ]
+            };
+
+            var jsonIn = DebugTool.ToJson(cTypes);
+            string jsonOut;
+
+            using (var fileOut = File.CreateText(tmp))
+                XmlTool.Write(cTypes, fileOut);
+
+            using (var fileIn = File.OpenText(tmp))
+            {
+                var model = XmlTool.Read(fileIn);
+                Assert.Equal(2, model.Types.Count);
+
+                jsonOut = DebugTool.ToJson(model);
+            }
+
+            Assert.Equal(jsonIn, jsonOut);
+        }
+    }
+}
