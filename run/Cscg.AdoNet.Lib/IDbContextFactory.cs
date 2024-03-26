55f27045cb36712736b8bad8862c4f3d7c04ea90
xafero <xafero@users.noreply.github.com>
2024-03-26 20:59:20 +0100

    Adding context factory

---
+++ run/Cscg.AdoNet.Lib/IDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cscg.AdoNet.Lib
+{
+    public interface IDbContextFactory<out TContext>
+        where TContext : DbContext
+    {
+        TContext Create();
+    }
+
+    public sealed class DbContextFactory<TContext> : IDbContextFactory<TContext>
+        where TContext : DbContext
+    {
+        private readonly Func<TContext> _func;
+
+        public DbContextFactory(Func<TContext> func)
+        {
+            _func = func;
+        }
+
+        public TContext Create()
+        {
+            return _func();
+        }
+    }
+}
